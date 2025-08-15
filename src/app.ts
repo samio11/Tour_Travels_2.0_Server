7190f250ad809a149a3c90e4ec2bac1c20b29ab1
samio11 <samiohasan6@gmail.com>
2025-08-15 20:19:18 +0600

    Cors problem fixed

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,17 @@ import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import { notFound } from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
 import { rootRouter } from "./app/routes";
+import config from "./app/config";
 const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1", rootRouter);
-app.use(cors());
+app.use(
+  cors({
+    origin: config.FRONTEND_URL as string,
+    credentials: true,
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Server is running successfully" });
